3004bae9d1830c035ca08ff134016daff99f5500
Vugdi <vegard.lund@geanor.no>
2024-11-26 14:37:50 +0100

    Første utfylling av tekst i filene

---
+++ script.js
@@ -0,0 +1,11 @@
+// Initialiser kartet
+var map = L.map('map').setView([59.07019, 9.59538], 9.5); // Sett posisjon og zoom-nivå
+
+// Legg til OpenStreetMap som bakgrunn
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+// Legg til en marker
+var marker = L.marker([59.07019, 9.59538]).addTo(map);
+marker.bindPopup('<b>Hei, verden!</b>').openPopup();
