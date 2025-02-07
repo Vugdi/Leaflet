a7c0d8c451bc7a2c2a62c8051326e46c8b2d7c40
Vugdi <vegard.lund@geanor.no>
2025-02-07 10:31:57 +0100

    slettet nesten hele

--- script.js
+++ script.js
@@ -6,17 +6,7 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-fetch('data/berggrunn.geojson')
-    .then(response => {
-        console.log('GeoJSON response:', response);  // Logg responsen
-        return response.json();
-    })
-    .then(geojson => {
-        console.log('GeoJSON-data:', geojson);  // Logg GeoJSON-dataene
-    })
-    .catch(error => {
-        console.error('Feil ved lasting av GeoJSON:', error);
-    });
+
 
 // Legg til en marker
 var marker = L.marker([59.07019, 9.59538]).addTo(map);
